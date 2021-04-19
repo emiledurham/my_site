cb37519037509c0feb57a25f1d250c86ab75d8ea
Emile Durham <durhamemile@gmail.com>
2021-04-19 12:37:41 -0700

    second commit

---
+++ my_app_blog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyAppBlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_app_blog'
---
+++ my_app_blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+# Create your views here.
+
+
+def blog(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog/blog.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    detail_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'detail_blog': detail_blog})
