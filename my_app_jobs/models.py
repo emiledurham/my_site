c78caa5ead01f148ca355334ff2fd8629ba76d3e
Emile Durham <durhamemile@gmail.com>
2021-04-22 13:28:04 -0700

    add job url to models

--- my_app_jobs/models.py
+++ my_app_jobs/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    url = models.URLField(max_length=200, default='https://www.emiledurham.com/')
 
     def __str__(self):
         return self.summary
