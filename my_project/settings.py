aaf8f1be00f4bf5ba5f6775af66509b42c828c39
Emile Durham <durhamemile@gmail.com>
2021-04-22 12:44:28 -0700

    update .gitignore, resume troubleshoot

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('MY_PROJECT_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
